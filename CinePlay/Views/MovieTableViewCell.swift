9e4c489f4e0d00ff0de30d118adbc5e891a4ee62
wesley <wduarte_estag@horizonte.tv.br>
2017-08-29 14:58:59 -0300

    Creating MovieTableView

---
+++ CinePlay/Views/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  CinePlay
+//
+//  Created by wesley on 8/29/17.
+//  Copyright © 2017 wesley. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
